5e219b9159300586fc148e9b949d47820c19c460
taki-run <taki@example.com>
2026-02-25 17:50:12 +0900

    Initial commit of running blog project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My Running Log';
+export const SITE_DESCRIPTION = '日々のランニングの記録とコースの紹介です。';
